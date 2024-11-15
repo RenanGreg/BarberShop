9a8a1084eb5b059e6f063b567e8971dba70ca0d7
RenanGreg <renangreg@outlook.com>
2024-11-15 17:01:44 -0300

    first commit

---
+++ src/View/Main.java
@@ -0,0 +1,28 @@
+package View;
+
+import Model.Cliente;
+import Model.Servico;
+import Model.Usuario;
+
+
+public class Main { 
+    public static void main (String[] args){
+        
+        String nome = "Renan"; 
+        System.out.println(nome); 
+        
+        Servico servico = new Servico(1, "barba", 30);
+        
+        System.out.println(servico.getDescricao());
+        System.out.println(servico.getValor()); 
+        
+        
+        Cliente cliente = new Cliente(1, "Renan,", "Rua Rogerio Ceni", "4354564"); 
+        System.out.println(cliente.getNome());  
+        
+        
+        Usuario usuario = new Usuario(1, "Barbeiro", "senha");
+        System.out.println(usuario.getNome()); 
+    }
+    
+}
